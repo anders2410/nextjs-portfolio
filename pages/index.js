e194c8920de44747a430cc7ec2869629648f293d
Anders Holt Knudsen <andhk11@gmail.com>
2021-05-13 18:32:42 +0200

    Changed name of environmental variables

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,8 @@ export default function Projects({ projects }) {
 
 export async function getStaticProps() {
   const client = createClient({
-    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
   });
 
   const res = await client.getEntries({ content_type: "project" });
--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -3,8 +3,8 @@ import Image from "next/image";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 const client = createClient({
-  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 });
 
 export const Project = ({ project }) => {
