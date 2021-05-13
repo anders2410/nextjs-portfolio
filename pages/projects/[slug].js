287930876730836854a13ef9662daf2a4573b404
Anders Holt Knudsen <andhk11@gmail.com>
2021-05-13 18:15:53 +0200

    Added separate [slug]

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,3 +1,65 @@
-export default () => {
-  return <div>Recipe Details</div>
-}
+import { createClient } from "contentful";
+import Image from "next/image";
+
+const client = createClient({
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+});
+
+export default ({ project }) => {
+  const { featuredImage, title, date, tags, content } = project.fields;
+
+  return (
+    <div>
+      <Image
+        src={"https:" + featuredImage.fields.file.url}
+        width={featuredImage.fields.file.details.image.width}
+        height={featuredImage.fields.file.details.image.height}
+      />
+      <div>
+        <h2>{title}</h2>
+        <h4>{date}</h4>
+        <h3>Tags:</h3>
+        {tags.map((tag) => (
+          <span key={tag}>{tag} </span>
+        ))}
+      </div>
+      <div className="content">
+        <h3>Content</h3>
+      </div>
+    </div>
+  );
+};
+
+// This will create all the paths for our projects.
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({ content_type: "project" });
+
+  const paths = res.items.map((item) => {
+    return {
+      params: {
+        slug: item.fields.slug,
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+// This is called on each individual 'Project'
+export const getStaticProps = async ({ params }) => {
+  // Will always return an array
+  const { items } = await client.getEntries({
+    content_type: "project",
+    "fields.slug": params.slug,
+  });
+
+  return {
+    props: {
+      project: items[0],
+    },
+  };
+};
