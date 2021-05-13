07c65da93e2a2058779c7f785f3421fabe113497
Anders Holt Knudsen <andhk11@gmail.com>
2021-05-13 13:46:42 +0200

    Added new setup from 'The Net Ninja'

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,25 @@
-import {fetchEntries} from '../util/getContent'
-import Project from '../components/Project'
+import {createClient} from "contentful";
 
-export default ({projects}) => {
-  console.log(projects);
-  return (<div className="posts">
-    {projects.map((p) => {
-      return <Project key={p.date} date={p.date} title={p.title}/>
-    })}
-  </div>)
+export default function Projects({ projects }) {
+  console.log(projects)
+  return (
+    <div className="recipe-list">
+      Recipe List
+    </div>
+  )
 }
 
 export async function getStaticProps() {
-  const res = await fetchEntries()
-  const projects = await res.map((p) => {
-    return p.fields
+  const client = createClient({
+    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
   })
+
+  const res = await client.getEntries({content_type: 'project'});
 
   return {
     props: {
-      projects,
-    },
+      projects: res.items
+    }
   }
 }
