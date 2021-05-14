478dad2aa6fd9c5d5ac51592a8d8f8c1f29cb7d0
Anders Holt Knudsen <andhk11@gmail.com>
2021-05-14 12:03:32 +0200

    added custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const NotFound = () => {
+  const router = useRouter();
+
+  // Redirects the user to the 'Homepage' after 4 seconds.
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 4000);
+  }, []);
+
+  return (
+    <div className="not-found">
+      <h1>404</h1>
+      <h2>Ooops! That page cannot be found :(</h2>
+      <p>
+        Redirecting to the <Link href="/">Homepage</Link> to see my projects.
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
