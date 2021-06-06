9ac4611b4c99844cef0aa2b8eb2b31bcdb06c661
Anders Holt Knudsen <andhk11@gmail.com>
2021-06-06 21:00:22 +0200

    Added line-breaks and updated styling of [slug]

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from "contentful";
 import Image from "next/image";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { BLOCKS } from "@contentful/rich-text-types";
 import Skeleton from "../../components/Skeleton";
 import React from "react";
 
@@ -9,11 +10,29 @@ const client = createClient({
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 });
 
+const renderDocument = (document) => {
+  const Text = ({ children }) => <p>{children}</p>;
+
+  const options = {
+    renderNode: {
+      [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
+    },
+    renderText: (text) => {
+      return text.split("\n").reduce((children, textSegment, index) => {
+        return [...children, index > 0 && <br key={index} />, textSegment];
+      }, []);
+    },
+  };
+
+  return documentToReactComponents(document, options);
+};
+
 // This is a "nested route".
 const Project = ({ project }) => {
   if (!project) return <Skeleton />;
 
   const { featuredImage, title, date, tags, content } = project.fields;
+  console.log(content);
 
   return (
     <main className="bg-gray-200">
@@ -26,17 +45,17 @@ const Project = ({ project }) => {
             className="rounded"
           />
         </div>
-        <section className="bg-gray-100 rounded shadow-xl lg:flex p-20">
+        <section className="bg-gray-100 rounded shadow-xl lg:flex px-40 py-20">
           <div className="text-lg mr-20">
-            <h1 className="text-6xl text-gray-700 mb-4 font-bold">{title}</h1>
+            <h1 className="text-6xl text-gray-700 mb-6 font-bold">{title}</h1>
             <div className="text-gray-600 text-xl whitespace-pre-line">
-              {documentToReactComponents(content)}
+              {renderDocument(content)}
             </div>
             <div className="flex flex-wrap pt-2">
               {tags.map((tag) => (
                 <span
                   key={tag}
-                  className="text-sm font-medium bg-gray-200 py-1 px-2 rounded text-black align-middle mr-2 mb-2 font-bold"
+                  className="text-sm font-medium bg-gray-200 py-1 px-2 rounded text-black align-middle mr-2 mb-2 mt-10 font-bold"
                 >
                   {tag}
                 </span>
