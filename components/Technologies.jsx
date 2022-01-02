416ca25f3e4d0fe1a22cfcbdb89897616ee44d6f
Anders Holt Knudsen <andhk11@gmail.com>
2022-01-02 23:09:59 +0100

    updated text on "Technologies"

--- components/Technologies.jsx
+++ components/Technologies.jsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react'
 
 const Line = () => (
   <hr
     style={{
-      color: "white",
-      backgroundColor: "white",
+      color: 'white',
+      backgroundColor: 'white',
       height: 1,
       width: 150,
-      opacity: "50%",
-      marginBottom: "10px",
+      opacity: '50%',
+      marginBottom: '10px',
     }}
   />
-);
+)
 
 const Technologies = () => {
   return (
@@ -28,12 +28,12 @@ const Technologies = () => {
             Java
           </h2>
           <p className="text-white text-center text-xl font-light">
-            Java is my most experienced language. I&apos;ve made some major and
+            Java is my most experienced language. I have made some major and
             minor projects including my bachelor in &apos;Shortest Paths&apos;
             and an app &apos;HeavyMetalDK&apos;. The latter was made in Android
             Studio. This also includes making games like &apos;Minesweeper&apos;
             and &apos;Tic-Tac-Toe&apos;, which can be seen under my projects or
-            in my{" "}
+            in my{' '}
             <a
               href="https://github.com/anders2410"
               className="inline-ref underline"
@@ -41,19 +41,20 @@ const Technologies = () => {
               rel="noreferrer"
             >
               GitHub
-            </a>{" "}
+            </a>{' '}
             repo.
           </p>
         </div>
         <div className="p-6">
           <h2 className="text-2xl text-white text-center font-semibold mb-2">
-            Python
+            JavaScript/TypeScript
           </h2>
           <p className="text-white text-center text-xl font-light">
-            Is my second most experienced language. I enjoy using it as I think
-            it&apos;s concise and simplistic. I&apos;ve mostly used it in
-            smaller projects for data visualization, games or scripting, but
-            also in &apos;Machine Learning&apos; at University.
+            Is my second most experienced language. All my JavaScript knowledge
+            comes from my current job at &apos;Styrelsen for It og Læring&apos;.
+            I do not like JavaScript per se as I think it lacks some
+            type-safety. This is the reason why I am currently trying to learn
+            TypeScript through YouTube and Udemy, but also at work.
           </p>
         </div>
         <div className="p-6">
@@ -63,15 +64,14 @@ const Technologies = () => {
           <p className="text-white text-center text-xl font-light">
             I have picked up React when I began my work at &apos;Styrelsen for
             IT og Læring&apos;. This is our main frontend framework together
-            with JavaScript. I&apos;m very confident with the framework, and my
-            JavaScript skills are growing each day. I strive keep myself updated
-            with the latest trends in React like &apos;Hooks&apos;,
-            &apos;Router&apos; and so forth.
+            with JavaScript. I am very confident with the framework and strive
+            to keep myself updated with the latest trends in React like Hooks,
+            Router, NextJs, Vite and so forth.
           </p>
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Technologies;
+export default Technologies
